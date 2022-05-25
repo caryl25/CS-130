17276576875c2e14c1a1167ba643201094d58358
Cary Liu <caryliu2025@northwestern.edu>
2022-05-24 22:06:16 -0500

    hw2 ready for wave

--- homeworks/hw04/your_task/js/scripts.js
+++ homeworks/hw04/your_task/js/scripts.js
@@ -35,7 +35,7 @@ const getTracks = (term) => {
             for(const track of tracks) {
 
                 document.querySelector("#tracks").innerHTML +=
-                `<button class="track-item preview" data-preview-track="${track.preview_url}">
+                `<button class="track-item preview" data-preview-track="${track.preview_url}" onclick="handleTrackClick(event)";>
                     <img src="${track.album.image_url}">
                     <i class="fas play-track fa-play" aria-hidden="true"></i>
                     <div class="label">
@@ -45,16 +45,45 @@ const getTracks = (term) => {
                         </p>
                     </div>
                 </button>`
+                
+
             }
         });
 };
 
 const getAlbums = (term) => {
+    document.querySelector("#albums").innerHTML = "";
     console.log(`
         get albums from spotify based on the search term
         "${term}" and load them into the #albums section 
         of the DOM...`);
+        
+        fetch("https://www.apitutor.org/spotify/simple/v1/search?type=album&q="+term)
+        .then(response => response.json())
+        .then(albumses => {
+            console.log(albumses);
+            if(albumses.length === 0) {
+                document.querySelector("#albums").innerHTML += `
+                <p>No albums found for "${term}"</p>`;
+            }
+            for(const album of albumses) {
+
+                document.querySelector("#albums").innerHTML +=
+                `<section class="album-card" id="${album.id}">
+                    <div>
+                        <img src="${album.image_url}">
+                        <h2>${album.name}</h2>
+                        <div class="footer">
+                            <a href="${album.spotify_url}" target="_blank">
+                                view on spotify
+                            </a>
+                        </div>
+                    </div>
+                </section>`
+            }
+        });
 };
+
 
 const getArtist = (term) => {
     document.querySelector("#artist").innerHTML = "";
@@ -73,7 +102,7 @@ const getArtist = (term) => {
             for(const artist of artists) {
 
                 document.querySelector("#artist").innerHTML +=
-                `<section class="artist-card" id="3Nrfpe0tUJi4K4DXYWgMUX">
+                `<section class="artist-card" id="${artist.id}">
                     <div>
                         <img src="${artist.image_url}">
                         <h2>${artist.name}</h2>
@@ -89,9 +118,24 @@ const getArtist = (term) => {
 };
 
 const handleTrackClick = (ev) => {
-    const previewUrl = ev.currentTarget.getAttribute('data-preview-track');
+    const currentTrack = ev.currentTarget;
+    console.log(currentTrack);
+    document.querySelector("#current-track").innerHTML = `
+        <img src="${currentTrack.querySelector("img").getAttribute("src")}">
+        <i class="fas play-track fa-pause" aria-hidden="true"></i>
+        <div class="label">
+            <h2>${currentTrack.querySelector("div h2").innerHTML}</h2>
+            <p>
+                ${currentTrack.querySelector("div p").innerHTML}
+            </p>
+        </div>
+    `;
+    const previewUrl = currentTrack.getAttribute("data-preview-track");
     console.log(previewUrl);
-}
+    audioPlayer.setAudioFile(previewUrl);
+    audioPlayer.play();
+
+};
 
 document.querySelector('#search').onkeyup = (ev) => {
     // Number 13 is the "Enter" key on the keyboard
